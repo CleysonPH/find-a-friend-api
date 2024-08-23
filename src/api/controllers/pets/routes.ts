a04ca8b06acb7449d414961607e3a57490d0cfd4
Cleyson Lima <cleysonph@gmail.com>
2024-08-23 10:08:56 -0300

    add search pets route

--- src/api/controllers/pets/routes.ts
+++ src/api/controllers/pets/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { verifyJWT } from '@/api/middlewares/verify-jwt'
+import { search } from './search'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('', { onRequest: [verifyJWT] }, create)
+  app.get('', search)
 }
